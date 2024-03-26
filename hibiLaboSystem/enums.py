e95be955bbe57713264b07083d6c8b0b22ee2bdc
TRAN VAN HUY <huy1821051049@gmail.com>
2024-03-26 13:25:03 +0700

    check enum

--- hibiLaboSystem/enums.py
+++ hibiLaboSystem/enums.py
@@ -3,11 +3,11 @@ from enum import Enum
 
 class RoleEnum(Enum):
 
-	日々研 = "日々研"
-	Partner = "Partner"
-	Company_Admin = "Company Admin"
-	Company_SuperVisor = "Company SuperVisor"
-	Company_Staff = "Company Staff"
+	日々研 = 90
+	Partner = 40
+	Company_Admin = 30
+	Company_SuperVisor = 20
+	Company_Staff = 10
 	@classmethod
 	def choices(cls):
 		return [(i, i.value) for i in cls]
