3cc8f45d3218ab6fc7853544bba012459081d6ee
TRAN VAN HUY <huy1821051049@gmail.com>
2024-05-06 10:56:07 +0700

    #248 commit

--- hibiLaboSystem/admin.py
+++ hibiLaboSystem/admin.py
@@ -1,41 +1,113 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from import_export.admin import ImportMixin
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.core.exceptions import ValidationError
 from . import forms, utils, validate
-from django.forms import CheckboxSelectMultiple
+from django.forms import CheckboxSelectMultiple, SelectMultiple, TextInput
 from django.db import models
 from .models import *
 from django.contrib.auth.admin import UserAdmin
 from django.db.models import Q
 import json
 from django.core.cache import cache
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.shortcuts import redirect
+import datetime
+import threading
+import jaconv
 from django.utils.html import format_html
+
+thread_local = threading.local()
 
 
 # Register your models here.
 CustomeUser = get_user_model()
 class HonneEvaluationPeriodInline(admin.TabularInline):
     model = HonneEvaluationPeriod
+    fields = ["evaluation_name", "evaluation_start", "evaluation_end", "honne_questions"]
     extra = 0  # Number of empty forms to display
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
+    readonly_fields=[]
+    
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+    
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_superuser
+        
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:   
+            self.readonly_fields = ["evaluation_name", "honne_questions"]
+        return  self.readonly_fields
+            
+    def get_queryset(self, request):
+        qs = super(HonneEvaluationPeriodInline, self).get_queryset(request)
+        if not request.user.is_superuser:
+            today = datetime.date.today()
+            return qs.filter(evaluation_start__lte=today, evaluation_end__gte=today)
+        return qs
+
+
 class SelfcheckEvaluationPeriodInline(admin.TabularInline):
     model = SelfcheckEvaluationPeriod
+    fields = ["evaluation_name", "evaluation_start", "evaluation_end", "selfcheck_questions"]
     extra = 0  # Number of empty forms to display
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
+    readonly_fields = []
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+    
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            self.readonly_fields = ["evaluation_name", "selfcheck_questions"]
+        return self.readonly_fields
+    
+    def get_queryset(self, request):
+        qs = super(SelfcheckEvaluationPeriodInline, self).get_queryset(request)
+        if not request.user.is_superuser:
+            today = datetime.date.today()
+            return qs.filter(evaluation_start__lte=today, evaluation_end__gte=today)
+        return qs
+    
 class BonknowEvaluationPeriodInline(admin.TabularInline):
     model = BonknowEvaluationPeriod
+    fields = ["evaluation_name", "evaluation_start", "evaluation_end", "respons_questions", "think_questions"]
     extra = 0  # Number of empty forms to display
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
+    readonly_fields = []
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+            
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            self.readonly_fields = ["evaluation_name", "respons_questions", "think_questions"]
+        return self.readonly_fields
+        
+    def get_queryset(self, request):
+        qs = super(BonknowEvaluationPeriodInline, self).get_queryset(request)
+        if not request.user.is_superuser:
+            today = datetime.date.today()
+            return qs.filter(evaluation_start__lte=today, evaluation_end__gte=today)
+        return qs
+    
 class UserInline(admin.TabularInline):
     model = User
     fields = ['username', 'first_name', 'last_name', 'email', 'role']
@@ -57,22 +129,54 @@ class MandaraPeriosInline(admin.TabularInline):
     # formfield_overrides = {
     #     models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     # }
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+    
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_superuser
+    
+    def get_queryset(self, request):
+        qs = super(MandaraPeriosInline, self).get_queryset(request)
+        if not request.user.is_superuser:
+            today = datetime.date.today()
+            return qs.filter(start_date__lte=today, end_date__gte=today)
+        return qs 
 
 class WatasheetInline(admin.TabularInline):
     model = WatasheetEvaluationPeriod
-    # fields = ['start_date', 'end_date', 'company_id']
+    fields = ["evaluation_name", "evaluation_start", "evaluation_end", "watasheet_questions"]
     extra = 0  # Number of empty forms to display
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
+    readonly_fields = []
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+    
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            self.readonly_fields = ["evaluation_name", "watasheet_questions"]
+        return self.readonly_fields
+
+    def get_queryset(self, request):
+        qs = super(WatasheetInline, self).get_queryset(request)
+        if not request.user.is_superuser:
+            today = datetime.date.today()
+            return qs.filter(evaluation_start__lte=today, evaluation_end__gte=today)
+        return qs
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', ]
     exclude = ["created_by", "team_action_1_year", "team_action_5_years", "team_action_10_years", ]
-    inlines = [HonneEvaluationPeriodInline, SelfcheckEvaluationPeriodInline, WatasheetInline, BonknowEvaluationPeriodInline, MandaraPeriosInline, UserInline]
+    inlines = [HonneEvaluationPeriodInline, SelfcheckEvaluationPeriodInline, WatasheetInline, BonknowEvaluationPeriodInline, MandaraPeriosInline]
     def save_model(self, request, obj, form, change):
         if not change:
             obj.created_by = request.user.id
+        
         obj.save()
 
     def get_queryset(self, request):
@@ -85,9 +189,12 @@ class CompanyAdmin(admin.ModelAdmin):
         cache.clear()
         if not request.user.is_superuser:
             self.exclude = ["created_by", "team_action_1_year", "team_action_5_years", "team_action_10_years", "name", "date_start", "date_end", "active_flag", "partner"]
-            self.inlines = []
+            # self.inlines = []
         form = super(CompanyAdmin,self).get_form(request, obj, **kwargs)
-        return form
+        return form   
+
+    # def formfield_for_manytomany(self, db_field, request, **kwargs):
+    #     print(db_field.name)     
 
 class SelfcheckQuestionCustom(admin.ModelAdmin):
     list_filter = ["selfcheck_roles", "selfcheck_industries"]
@@ -106,9 +213,11 @@ class RoleCustom(admin.ModelAdmin):
 @admin.register(User)
 class UsersAdmin(ImportMixin,admin.ModelAdmin):
     list_display = ["id","username", "company", "branch", "role"]
-    list_filter = ['company']
-    exclude = ['created_by']
-    
+    list_filter = ['company',]
+    exclude = ['created_by', ]
+    actions = []
+    success = True
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "branch":
             if request.user.is_superuser:
@@ -117,16 +226,36 @@ class UsersAdmin(ImportMixin,admin.ModelAdmin):
                 if request.user.company_id:
                     kwargs["queryset"] = Branch.objects.filter(company_id=request.user.company_id)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
+   
 
     def save_model(self, request, obj, form, change):
         if not change:
             obj.created_by = request.user.id
-        obj.save()
-
-    def get_form(self, request, obj=None, **kwargs):
-        cache.clear()
+            obj.password = make_password(obj.password)
+        # else:
+        #     user = User.objects.filter(id=obj.id).first()
+        #     if user.password != obj.password:
+        #         obj.password = make_password(obj.password)
+        
         if not request.user.is_superuser:
-            self.exclude = ["user_permissions", "is_superuser", "is_active",'created_by']
+            obj.company_id = request.user.company_id
+            obj.add(request.user.company_id)
+
+        obj.save()
+               
+    def get_form(self, request, obj=None, **kwargs):
+
+        cache.clear()
+        obj = obj.id if obj else False
+
+        if obj:
+            if not request.user.is_superuser:
+                self.exclude = ["user_permissions", "is_superuser", "is_active",'created_by', 'company', 'password', ]
+
+        else:
+            if not request.user.is_superuser:
+                self.exclude = ["user_permissions", "is_superuser", "is_active",'created_by', 'company', ]
+
         form = super(UsersAdmin,self).get_form(request, obj, **kwargs)
         return form
 
@@ -140,8 +269,10 @@ class UsersAdmin(ImportMixin,admin.ModelAdmin):
 
     def changelist_view(self, request, extra_context=None):
         cache.clear()
+        
         if not request.user.is_superuser:
-            self.list_display = ["id", "username", "branch"]
+            self.list_display = ["id", "username", "branch", "role", ]
+            self.actions = ['update_branch']
         return super(UsersAdmin, self).changelist_view(request, extra_context)
 
     def import_action(self,request):
@@ -176,43 +307,43 @@ class UsersAdmin(ImportMixin,admin.ModelAdmin):
                 else:
                     company_id = company_id
 
-                branch_code = row[util_obj.get_column("branch_code")]
-                
+                branch_id = row[util_obj.get_column("branch_id")]
+
                 if User.objects.filter(username=username).exists() or username in array_user:
-                    import_object_status.append({"username": username, "company": company_id, "branch": branch_code, "status": "ERROR",
+                    import_object_status.append({"username": username, "company": company_id, "branch": branch_id, "status": "ERROR",
                                                 "msg": "username already exist!"})
                                                 
                 elif Company.objects.filter(id=company_id).first() is None:
-                    import_object_status.append({"username": username, "company": company_id, "branch": branch_code, "status": "ERROR",
+                    import_object_status.append({"username": username, "company": company_id, "branch": branch_id, "status": "ERROR",
                                             "msg": "company is not already exist!"}) 
                    
-                elif Branch.objects.filter(code=branch_code, company_id=company_id).first() is None:
-                    import_object_status.append({"username": username, "company": company_id, "branch": branch_code, "status": "ERROR",
+
+                elif Branch.objects.filter(id=branch_id ).first() is None:
+                    import_object_status.append({"username": username, "company": company_id, "branch": branch_id, "status": "ERROR",
                                             "msg": "Branch is not already exist!"})
                    
-                elif Company.objects.filter(id=company_id).first().id != Branch.objects.filter(code=branch_code, company_id=company_id).first().company.id:
-                    import_object_status.append({"username": username, "company": company_id, "branch": branch_code, "status": "ERROR",
+                elif Company.objects.filter(id=company_id).first().id != Branch.objects.filter(id=branch_id).first().company.id:
+                    import_object_status.append({"username": username, "company": company_id, "branch": branch_id, "status": "ERROR",
                                             "msg": "The branch must belong to the company!"})
                 
                 else:
-                    brach_id = Branch.objects.filter(code=branch_code, company_id=company_id).first().id
                     array_user.append(username)
                     try:
                         for validator in validators:
                             validator().validate(password)
                         create_new_characters.append(
                             User(
-                                username=username, password=make_password(password), created_by=request.user.id, first_name=first_name, last_name=last_name, company_id=company_id, branch_id=brach_id, role_id=role_id
+                                username=username, password=make_password(password), created_by=request.user.id, first_name=first_name, last_name=last_name, company_id=company_id, branch_id=branch_id, role_id=role_id
                             )
                         )
                         if request.user.is_superuser:
-                            import_object_status.append({"username": username, "company": company_id, "branch": branch_code,"status": "FINISHED",
+                            import_object_status.append({"username": username, "company": company_id, "branch": branch_id,"status": "FINISHED",
                                                         "msg": "User created successfully!"})
                         else:
-                            import_object_status.append({"username": username,"branch": branch_code, "status": "FINISHED",
+                            import_object_status.append({"username": username,"branch": branch_id, "status": "FINISHED",
                                                         "msg": "User created successfully!"})
                     except ValidationError as e:
-                        import_object_status.append({"username": username, "company": company_id, "branch": branch_code, "status": "ERROR",
+                        import_object_status.append({"username": username, "company": company_id, "branch": branch_id, "status": "ERROR",
                                                 "msg": str(e.args[0])})
                         
             # bulk create objects
@@ -226,12 +357,24 @@ class UsersAdmin(ImportMixin,admin.ModelAdmin):
             return HttpResponse(json.dumps(context), content_type="application/json")
         # print(make_password('123456'));
         form = forms.CsvImportForm()
-   
-        context = {"form": form, "form_title": "Upload users csv file.",
+        if not request.user.is_superuser:
+            context = {
+                "form": form, 
+                "form_title": "Upload users csv file.",
                 "description": "The file should have following headers: "
-                                "[username,first_name,last_name,password, company_id, branch_code]."
+                                "[username,first_name,last_name,password, branch_id]."
                                 " The Following rows should contain information for the same.",
-                                "endpoint": "/admin/hibiLaboSystem/user/import/"}
+                                "endpoint": "/admin/hibiLaboSystem/user/import/",
+            }
+        else: 
+            context = {
+                "form": form, 
+                "form_title": "Upload users csv file.",
+                "description": "The file should have following headers: "
+                                "[username,first_name,last_name,password, company_id, branch_id]."
+                                " The Following rows should contain information for the same.",
+                                "endpoint": "/admin/hibiLaboSystem/user/import/"
+            }
 
         return render(
             request, "admin/import_users.html", context
@@ -241,6 +384,24 @@ class UsersAdmin(ImportMixin,admin.ModelAdmin):
         if not request.user.is_superuser:
             return qs.filter(Q(is_superuser=False) & (Q(created_by=request.user.id) | Q(company_id=request.user.company_id)))
         return qs
+
+    def update_branch(self, request, queryset):
+        if request.user.is_superuser:
+            return HttpResponseRedirect(request.get_full_path())
+            
+        branches = Branch.objects.filter(company_id=request.user.company.id)
+        if 'apply' in request.POST:
+            queryset.update(branch_id=request.POST['branch'])
+            
+            self.message_user(request,
+                              "ユーザー {} 人の支店が変更されました。".format(queryset.count()))
+            return HttpResponseRedirect(request.get_full_path())
+                        
+        return render(request,
+                      'admin/select_branch.html',
+                      context={'users':queryset, 'branches': branches})
+
+    update_branch.short_description = "新しい支店を選択"
 
 class BranchAdmin(admin.ModelAdmin):
     list_filter = ['company']
