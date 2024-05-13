51b6985b79f2abe41b79ca6033b59212c1b8e0e0
TRAN VAN HUY <huy1821051049@gmail.com>
2024-05-13 17:36:07 +0700

    #287 commit

--- hibiLaboSystem/static/js/bonknow-think.js
+++ hibiLaboSystem/static/js/bonknow-think.js
@@ -21,6 +21,7 @@ const chart = new Chart(mycanvas, {
         ]
     },
     options: {
+        barThickness: 100,
         // events: [],
         plugins: {
             datalabels: {
@@ -55,8 +56,11 @@ const chart = new Chart(mycanvas, {
                 stacked: true,
                 ticks: {
                     color: '#000000',
-                    font: { size: window.innerWidth <= 500 ? 12 : 20 }
-                }
+                    font: { size: window.innerWidth <= 500 ? 12 : 20 },
+                    barThickness: 100
+                },
+                min: 0,
+                max: 100,
             }
         },
         elements: {
