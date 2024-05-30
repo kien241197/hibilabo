ee99eb7fc49da4d99fb65318fb5639022c738e22
TRAN VAN HUY <huy1821051049@gmail.com>
2024-05-30 16:06:33 +0700

    #200 commit

--- static/js/info-update.js
+++ static/js/info-update.js
@@ -5,18 +5,26 @@ $(document).ready(function () {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
+            console.log("vao day")
             reader.onload = function (e) {
                 $('#image-preview').attr('src', e.target.result);
                 $('#image-preview-1').attr('src', e.target.result).css('display', 'block')
+                $('#delete-image').css('display', 'block')
+
             };
 
             reader.readAsDataURL(input.files[0]);
         }
     }
 
-    $(document).ready(function () {
-        $('#id_image').change(function () {
-            readURL(this);
-        });
+    $('#id_image').change(function () {
+        readURL(this);
     });
+
+    $('#delete-image').click(function () {
+
+        $('#image-preview-1').attr('src', '').css('display', 'none')
+        $(this).css('display', 'none')
+        $("#id_image").val("")
+    })
 })
