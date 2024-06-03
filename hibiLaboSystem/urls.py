47b80ad99af9fcb04d38a15c01aa99ab18be1ebd
TRAN VAN HUY <huy1821051049@gmail.com>
2024-06-03 15:44:39 +0700

    #test-print commit

--- hibiLaboSystem/urls.py
+++ hibiLaboSystem/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
 	path('bonknow-respons', views.BonknowRespons.as_view(), name='bonknow_respons'),
 
 	path('mandara-print/<id>', views.mandara_pdf, name='mandara_print'), #print PDF
+	path('test-mandara-print/<id>', views.test_mandara_pdf, name='mandara_print'), #print PDF
+    
     path('mandara-sheet', views.MandaraSheet.as_view(), name='mandara_sheet'), #Display 2
     path('mandata-create', views.MandaraCreate.as_view(), name='mandara_create'), #Display 3
     path('mandara-reuse', views.MandaraReuse.as_view(), name='mandara_reuse'), #Display 4
