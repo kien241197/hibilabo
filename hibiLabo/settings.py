fbcac9849c6eeb442edb35439c0be13dcf77d0e2
Tran-VanHuy <huy1821051049@gmail.com>
2024-09-05 10:56:14 +0700

    #452 update

--- hibiLabo/settings.py
+++ hibiLabo/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = 'static'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/staic/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- static/js/info-update.js
+++ static/js/info-update.js
@@ -2,48 +2,42 @@ $(document).ready(function () {
 
     function readURL(input) {
         if (input.files && input.files[0]) {
-            var file = input.files[0];
-            var fileType = file.type;
-            var validImageTypes = ["image/jpeg", "image/png"];
-
-            if ($.inArray(fileType, validImageTypes) < 0) {
-                alert("Please select a valid image file (jpg, png).");
-                $('#id_image').val("");
-                return;
-            }
-
             var reader = new FileReader();
-            console.log("reader", reader);
 
             reader.onload = function (e) {
                 $('#image-preview').attr('src', e.target.result);
-                $('#image-preview-1').attr('src', e.target.result).css('display', 'block');
-                $('#delete-image').css('display', 'block');
+                $('#image-preview-1').attr('src', e.target.result).css('display', 'block')
+                $('#delete-image').css('display', 'block')
+
             };
 
-            reader.readAsDataURL(file);
+            reader.readAsDataURL(input.files[0]);
         }
     }
 
     $('#id_image').change(function () {
-        if ($(this).val()) {
+        if($(this).val()){
+
             readURL(this);
+
         } else {
-            deleteImage();
+            deleteImage()
         }
     });
 
     $('#delete-image').click(function () {
-        deleteImage();
-    });
+
+        deleteImage()
+    })
 
     function deleteImage() {
-        $('#image-preview').attr('src', image);
-        $('#delete-image').css('display', 'none');
-        $("#id_image").val("");
+
+        $('#image-preview').attr('src', image)
+        $('#delete-image').css('display', 'none')
+        $("#id_image").val("")
 
         if (!image) {
-            $('#image-preview-1').attr('src', image).css('display', 'none');
+            $('#image-preview-1').attr('src', image).css('display', 'none')
         }
     }
-});
+})
